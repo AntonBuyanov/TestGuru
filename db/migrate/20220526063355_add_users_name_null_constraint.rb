72660e267bcc9e4ddcd5c7da0a5e3a41a69dfd4b
Anton <anton6024@gmail.com>
2022-05-26 13:04:59 +0600

    feat: Rails models
    Create models: answer, category, question, test, user. Migrations to limit integrity. Default values in tables: tests, answers

---
+++ db/migrate/20220526063355_add_users_name_null_constraint.rb
@@ -0,0 +1,12 @@
+class AddUsersNameNullConstraint < ActiveRecord::Migration[6.1]
+  # def change
+  # end
+
+  def up
+    change_column_null(:users, :name, false)
+  end
+
+  def down
+    change_column_null(:users, :name, true)
+  end
+end
