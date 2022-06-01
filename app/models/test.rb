79e07f453208704fd281770b545c52846e7bfe35
Anton <anton6024@gmail.com>
2022-06-01 21:49:42 +0600

    feat: ActiveRecord query interface
    Create model UserPassedTest. Initial data in seeds.rb Class method model Test. Instance method model User.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.find_test_by_category(name_category)
+    Test.where(categories: { title: name_category }).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def level_test(level)
+    Test.where(user_passed_test: { user_id: user_id, level: level})
+  end
 end
