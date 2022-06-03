1369474e86160cd75555c0229f1931d1a1838617
Anton <anton6024@gmail.com>
2022-06-03 13:55:34 +0600

    fix: ActiveRecord query interface
    Test method outputs an array of test names. Fix method User

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
 
   def self.find_test_by_category(name_category)
     joins('JOIN categories ON categories.id = tests.category_id')
-      .where(categories: { title: name_category }).order(:title)
+      .where(categories: { title: name_category }).order(:title).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   def level_test(level)
     Test.joins('JOIN user_passed_tests ON tests.id = user_passed_tests.test_id')
-      .where('user_passed_tests.user_id = ? AND tests.level = ?', id, level)
+        .where(user_passed_tests: { user_id: :id }, tests: { level: level })
   end
 end
