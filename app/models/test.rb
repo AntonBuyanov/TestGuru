2a8f71704e5594f193ab644498246adfb9e408b5
Anton <anton6024@gmail.com>
2022-06-04 10:19:33 +0600

    fix: ActiveRecord query interface

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,8 @@ class Test < ApplicationRecord
 
   def self.find_test_by_category(name_category)
     joins('JOIN categories ON categories.id = tests.category_id')
-      .where(categories: { title: name_category }).order(:title).pluck(:title)
+      .where(categories: { title: name_category })
+      .order(:title)
+      .pluck(:title)
   end
 end
--- db/migrate/20220601145632_create_user_passed_tests.rb
+++ db/migrate/20220601145632_create_user_passed_tests.rb
@@ -1,8 +1,8 @@
 class CreateUserPassedTests < ActiveRecord::Migration[6.1]
   def change
     create_table :user_passed_tests do |t|
-      t.references :user, foreign_key: true
-      t.references :test, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :test, null: false, foreign_key: true
 
       t.timestamps
     end
