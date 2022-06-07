220ece14818f08637c838d6d5435b76bd8844cd0
Anton <anton6024@gmail.com>
2022-06-07 17:24:25 +0600

    feat: Active Record Associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :user_passed_tests
+  has_many :users, through: :user_passed_tests
 
   def self.find_test_by_category(name_category)
     joins('JOIN categories ON categories.id = tests.category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :user_passed_tests
+  has_many :tests, through: :user_passed_tests
   def level_test(level)
     Test.joins('JOIN user_passed_tests ON tests.id = user_passed_tests.test_id')
         .where(user_passed_tests: { user_id: :id }, tests: { level: level })
