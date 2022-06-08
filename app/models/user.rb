4b2f3519b915af9d99429f00af7e7c601f0cc9d0
Anton <anton6024@gmail.com>
2022-06-08 13:22:08 +0600

    fix: Active Record Associations
    Fix associations author test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_passed_tests, dependent: :destroy
   has_many :tests, through: :user_passed_tests
+  has_many :author_test, class_name: 'Test'
 
   def level_test(level)
     tests.where(level: level)
