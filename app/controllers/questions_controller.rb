40465823798297ca65cc11e7d0a2d6f2c1d7a4a2
Anton <anton6024@gmail.com>
2022-06-29 17:16:53 +0600

    feat: Rails Views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_question, only: :show
+  before_action :find_question, only: %i[show destroy]
   before_action :find_test, except: :show
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -16,22 +16,35 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = @test.questions.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
   def create
-    question = @test.questions.new(question_params)
+    @question = @test.questions.new(question_params)
 
-    if question.save
-      render plain: 'Question create'
+    if @question.save
+      redirect_to test_path(@test)
     else
       render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to test_path(@question.test)
+    else
+      render :edit
     end
   end
 
   def destroy
     @question.destroy
 
-    render plain: 'Question deleted'
+    redirect_to test_path(@question.test)
   end
 
   private
