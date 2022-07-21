ce4de2b936034b4fdad82455fbbf7a9b2ee1e306
Anton <anton6024@gmail.com>
2022-07-21 16:38:07 +0600

    feat: Twitter Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def github(user, repo)
     link_to user, "https://github.com/#{user}/#{repo}", target: '_blank'
   end
+
+  def flash_class(level)
+    case level
+    when :notice then "alert-info"
+    when :success then "alert-success"
+    when :error then "alert-error"
+    end
+  end
 end
